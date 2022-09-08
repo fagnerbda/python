bf16a02a999e5cdc0085be6ff1ad230dc97eb251
fagnerbda <89170942+fagnerbda@users.noreply.github.com>
2022-09-08 11:36:10 -0400

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,23 @@
+entrada = input("Digite um número")
+if(entrada == 0):
+  print("Número digitado foi 0")
+elif(entrada ==1):
+  print("Número digitado foi 1")
+elif(entrada ==2):
+  print("Número digitado foi 2")
+elif(entrada ==3):
+  print("Número digitado foi 3")
+elif(entrada ==4):
+  print("Número digitado foi 4")
+elif(entrada ==5):
+  print("Número digitado foi 5")
+elif(entrada ==6):
+  print("Número digitado foi 6")
+elif(entrada ==7):
+  print("Número digitado foi 7")
+elif(entrada ==8):
+  print("Número digitado foi 8")
+elif(entrada == 9):
+  print("Número digitado foi 9")
+else:
+  print("Número inválido")
